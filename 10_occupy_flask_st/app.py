4ebea4e983003397ff587dc0f2af01e809b03e39
DanielGelfand <dgelfand@stuy.edu>
2018-09-26 17:41:21 -0400

    updated

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -3,46 +3,14 @@ SoftDev1 pd<6>
 K<10> -- Jinja Tuning
 2018-09-24'''
 
-import csv #import csv module
-from random import randint
 from flask import Flask, render_template
-
-with open('occupations.csv','r') as csvfile: #opens occupations.csv as a readable file saved as variable csvfile
-    csvreader = csv.reader(csvfile) #reads the whole file
-    next(csvreader) # doesn't read the header
-    dict = {rows[0]:float(rows[1]) for rows in csvreader} #creates a dictionary where every even row is a key and every odd is a value
-    dict.pop('Total') # removes the Total key from dict
-
+from util import ants
 app = Flask(__name__) #create instance of class Flask
-
-def randJob():
-
-
-    #select an integer from a range of 1-1000
-    #If a job percentage is 5%, it takes up 50 spots in that range
-    rand = randint(1,1000)
-
-    lower = 0
-    upper = 0
-    # loop through the jobs
-    for key in dict:
-
-        # update the upper limit
-        # multiplication by 10 to get rid of decimals
-        upper += (dict[key]*10 + lower)
-
-        # check if random int is within that job percentage range
-        if(rand > lower and rand <= upper):
-            return key
-        else:
-        #update the lower limit(placed here bc for first iteration lower should stay 0)
-            lower += (dict[key]*10)
-
 
 @app.route("/") #assign fxn to route
 def hello_world():
     print(__name__)
-    return render_template('test.html', collection = dict,randJob = randJob() , ctr = 0)
+    return render_template('test.html', collection = ants.dict,randJob = ants.randJob())
 
 
 @app.route("/stuy")
---
+++ 10_occupy_flask_st/util/ants.py
@@ -0,0 +1,29 @@
+import csv #import csv module
+from random import randint
+
+with open('data/occupations.csv','r') as csvfile: #opens occupations.csv as a readable file saved as variable csvfile
+    csvreader = csv.reader(csvfile) #reads the whole file
+    next(csvreader) # doesn't read the header
+    dict = {rows[0]:float(rows[1]) for rows in csvreader} #creates a dictionary where every even row is a key and every odd is a value
+    dict.pop('Total') # removes the Total key from dict
+
+def randJob():
+    #select an integer from a range of 1-1000
+    #If a job percentage is 5%, it takes up 50 spots in that range
+    rand = randint(1,1000)
+
+    lower = 0
+    upper = 0
+    # loop through the jobs
+    for key in dict:
+
+        # update the upper limit
+        # multiplication by 10 to get rid of decimals
+        upper += (dict[key]*10 + lower)
+
+        # check if random int is within that job percentage range
+        if(rand > lower and rand <= upper):
+            return key
+        else:
+        #update the lower limit(placed here bc for first iteration lower should stay 0)
+            lower += (dict[key]*10)
